6e6a11ecdb017d7fab7f18e90d3d158ca3e0c7ba
AioiLight <info@aioilight.space>
2020-03-12 18:18:36 +0900

    プロジェクト ファイルを追加します。

---
+++ UninstallBlockerInstaller/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace UninstallBlockerInstaller
+{
+    internal static class Constants
+    {
+        /// <summary>
+        /// パッケージ名
+        /// </summary>
+        internal static readonly string Package = "jp.co.shiosefine.touch2ownertools";
+        
+        /// <summary>
+        /// クラス名
+        /// </summary>
+        internal static readonly string Class = "jp.co.shiosefine.touch2ownertools.MyDeviceOwnerReceiver";
+    }
+}
